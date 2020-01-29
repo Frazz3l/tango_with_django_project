f093b6057f6c100485e32cd70e67e42a13ef16a3
Fraser Paterson <2315720p@student.gla.ac.uk>
2020-01-29 13:25:16 +0000

    added template directory to settings.py

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,11 +14,13 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 print(__file__)
 print(os.path.dirname(__file__))
-print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
